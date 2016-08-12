61a32632490314f3a35701e810801fe023eee6e8
yoyo <18829290140@163.com>
2016-08-12 13:12:32 +0800

    frame

--- public/entry.jsx
+++ public/entry.jsx
@@ -1,11 +1,34 @@
-import Hello from './hello.jsx';
+// import Hello from './hello.jsx';
+import Login from './login.jsx';
+import App from './app.jsx';
+import Main from './main.jsx';
+import MealInfo from './mealinfo.jsx';
+import Order from './order.jsx';
+
+import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+require('jquery');
+require("bootstrap-webpack");
+require('./style.css');
+
+const router = <Router history={hashHistory}>
+  <Route path='/' component={App}>
+    <IndexRoute component={Login} />
+    <Route path='login' component={Login} />
+    <Route path='main' component={Main} />
+    <Route path='mealInfo' component={MealInfo} />
+    <Route path='order' component={Order}/>
+  </Route>
+</Router>;
+
+
+
 
 ReactDOM.render(
-  <Hello />,
-  document.getElementById("content")
+    router,
+    document.getElementById('content')
 );
 
 // use jquery
---
+++ public/main.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+export default class Main extends Component {
+    render() {
+        return <div><Link to='mealInfo'>这里是主页面</Link></div>
+    }
+}

e4302565826ad75f92b8346ba977c814af622d0c
baixue1016 <1773158403@qq.com>
2016-08-18 16:11:00 +0800

    add-api-validate

--- public/register.jsx
+++ public/register.jsx
@@ -9,7 +9,7 @@ export default class Register extends Component {
     super(props);
     this.state = {
       username: '',
-      password:''
+      password: ''
     }
   }
 
@@ -28,17 +28,21 @@ export default class Register extends Component {
             <img className="img-responsive center-block picture-head" src="./img/name1.png"/>
             <form onSubmit={this._onSubmit.bind(this)}>
               <div className="form-group register-user">
-                <input type="text" className="form-control" id="username" onBlur={checkUsername} placeholder="请输入8位学号" value={this.state.username} onChange={this._onUsernameChange.bind(this)}/>
+                <input type="text" className="form-control" id="username" onBlur={checkUsername} placeholder="请输入8位学号"
+                       value={this.state.username} onChange={this._onUsernameChange.bind(this)}/>
                 <div id="username-error" className=" tips"/>
               </div>
 
               <div className="form-group register-password">
-                <input type="password" className="form-control" id="password" onBlur={checkPassword} placeholder="请输入密码(6-16位)" value={this.state.password} onChange={this._onPasswordChange.bind(this)}/>
+                <input type="password" className="form-control" id="password" onBlur={checkPassword}
+                       placeholder="请输入密码(6-16位)" value={this.state.password}
+                       onChange={this._onPasswordChange.bind(this)}/>
                 <div id="password-error" className=" tips"/>
               </div>
 
               <div className="form-group register-password">
-                <input type="password" className="form-control" id="exampleInputPassword1" onBlur={checkConfirmPassword} placeholder="请确认密码"/>
+                <input type="password" className="form-control" id="exampleInputPassword1" onBlur={checkConfirmPassword}
+                       placeholder="请确认密码"/>
                 <div id="confirmPassword" className=" tips"/>
               </div>
               <button type="submit" className="btn btn-primary btn-block btn-register">注册</button>
@@ -50,13 +54,13 @@ export default class Register extends Component {
     )
   }
 
-  _onUsernameChange(event){
+  _onUsernameChange(event) {
     this.setState({
       username: event.target.value
     })
   }
 
-  _onPasswordChange(event){
+  _onPasswordChange(event) {
     this.setState({
       password: event.target.value
     })
@@ -66,14 +70,23 @@ export default class Register extends Component {
     event.preventDefault();
     request.post('/api/users')
       .send({
-        username:this.state.username,
-        password:this.state.password
+        username: this.state.username,
+        password: this.state.password
       })
-      .end((err,res) => {
+      .end((err, res) => {
 
-        if(err) return console.error(err);
-        //这里是前端处理  res：stateCode
-        alert(res);
+        if (err) return console.error(err);
+        if (res === 201) {
+          alert("注册成功！");
+          <Link to="main"/>
+        }
+        if (res === 400) {
+          alert("用户名或密码不正确！");
+        }
+        if (res === 409) {
+          alert("用户名已存在！");
+        }
+
       })
   }
 
